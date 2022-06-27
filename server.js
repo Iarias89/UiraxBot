ce323b12619bee53ef4353373e01e29094c9b9a1
Ismael <isma_ar_sa@hotmail.com>
2022-06-27 16:59:32 +0200

    1

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const ProductRouter = require("./api/ProductRouter");
 const UserRouter = require("./api/UserRouter");
 const CertificadoRouter = require("./api/CertificadoRouter");
 const fileUpload = require("express-fileupload");
- 
+const path = require ("path");
+
 const app = express();
 app.use(express.json()); //para que use archivos JSON meter datos por postman
 app.use(express.urlencoded({ extended: true})); //Pueda leer los datos dados por el postman
@@ -27,5 +28,10 @@ mongoose.connect(URI,{
 
 
 
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 app.listen(5000, () => console.log("server is run on port 5000"))
